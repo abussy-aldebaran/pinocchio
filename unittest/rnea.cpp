78de13b129a59d88b70ece9bdf4835d4be93981e
Megane Millan <megane.millan@inria.fr>
2024-10-08 09:26:17 +0200

    [test/rnea] Try fix for windows

--- unittest/rnea.cpp
+++ unittest/rnea.cpp
@@ -17,6 +17,7 @@
 #include "pinocchio/algorithm/center-of-mass.hpp"
 #include "pinocchio/algorithm/joint-configuration.hpp"
 #include "pinocchio/algorithm/crba.hpp"
+#include "pinocchio/algorithm/model.hpp"
 #include "pinocchio/algorithm/centroidal.hpp"
 #include "pinocchio/multibody/sample-models.hpp"
 #include "pinocchio/utils/timer.hpp"
